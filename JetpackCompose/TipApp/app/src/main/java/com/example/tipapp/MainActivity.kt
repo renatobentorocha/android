9fd5d42c99d0c7c4f2c1e310c4b45687686c1997
reanatorocha <87495675+reanatorocha@users.noreply.github.com>
2022-01-25 22:20:20 -0300

    (WIP) - Tip app

--- JetpackCompose/TipApp/app/src/main/java/com/example/tipapp/MainActivity.kt
+++ JetpackCompose/TipApp/app/src/main/java/com/example/tipapp/MainActivity.kt
@@ -3,28 +3,39 @@ package com.example.tipapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.tipapp.components.InputField
 import com.example.tipapp.ui.theme.TipAppTheme
 
+@ExperimentalComposeUiApi
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             App {
-                TopHeader()
+                Column {
+                    TopHeader()
+                    MainContent()
+                }
             }
         }
     }
@@ -37,11 +48,6 @@ fun App(content: @Composable () -> Unit){
             content()
         }
     }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
 }
 
 @Preview
@@ -70,6 +76,44 @@ fun TopHeader(totalPerPerson: Double = 123.0){
                 text = total,
                 style = MaterialTheme.typography.h4,
                 fontWeight = FontWeight.ExtraBold
+            )
+        }
+    }
+}
+
+@ExperimentalComposeUiApi
+@Preview
+@Composable
+fun MainContent(){
+    val valueState = remember {
+        mutableStateOf("")
+    }
+
+    val validState = remember(valueState.value) {
+        valueState.value.trim().isNotEmpty()
+    }
+
+    val keyBoardController = LocalSoftwareKeyboardController.current
+
+    Surface(
+        modifier = Modifier
+            .padding(2.dp)
+            .fillMaxWidth(),
+        shape = RoundedCornerShape(corner = CornerSize(8.dp)),
+        border = BorderStroke(
+            width = 1.dp,
+            color = Color.LightGray
+        )
+    ) {
+        Column {
+            InputField(
+                valueState = valueState,
+                labelId = "Enter Bill",
+                onAction = KeyboardActions {
+                    if (!validState) return@KeyboardActions
+
+                    keyBoardController?.hide()
+                }
             )
         }
     }

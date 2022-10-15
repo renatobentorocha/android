4fb64c9108c10d0b0cbbdba74adf79bd769d4afe
reanatorocha <87495675+reanatorocha@users.noreply.github.com>
2022-10-15 16:40:21 -0300

    Setting up

---
+++ JetpackCompose/app/src/main/java/com/example/jettrivia/di/AppModel.kt
@@ -0,0 +1,24 @@
+package com.example.jettrivia.di
+
+import com.example.jettrivia.network.QuestionAPI
+import com.example.jettrivia.util.Constants
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class AppModel {
+    @Singleton
+    @Provides
+    fun provideQuestionApi(): QuestionAPI = Retrofit
+        .Builder()
+        .baseUrl(Constants.BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+        .create(QuestionAPI::class.java)
+}

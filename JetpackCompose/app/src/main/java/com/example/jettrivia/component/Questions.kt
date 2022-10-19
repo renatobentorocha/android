3342f44063e60bd2f9d39b5e79bee24d5f70f82e
reanatorocha <87495675+reanatorocha@users.noreply.github.com>
2022-10-19 16:06:29 -0300

    Questions - dotted line

--- JetpackCompose/app/src/main/java/com/example/jettrivia/component/Questions.kt
+++ JetpackCompose/app/src/main/java/com/example/jettrivia/component/Questions.kt
@@ -1,6 +1,7 @@
 package com.example.jettrivia.component
 
 import android.util.Log
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Surface
@@ -8,6 +9,8 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.PathEffect
 import androidx.compose.ui.text.ParagraphStyle
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
@@ -34,6 +37,8 @@ fun Questions(viewModel: QuestionViewModel) {
 @Preview
 @Composable
 fun QuestionDisplay() {
+    val pathEffect = PathEffect.dashPathEffect(floatArrayOf(10f,10f), 0f)
+
     Surface(
         modifier = Modifier
             .fillMaxWidth()
@@ -47,11 +52,12 @@ fun QuestionDisplay() {
             horizontalAlignment = Alignment.Start
         ) {
             QuestionTracker()
+            DrawDottedLine(pathEffect)
         }
     }
 }
 
-//@Preview
+@Preview
 @Composable
 fun QuestionTracker(counter: Int = 10, outOf: Int = 100) {
     Text(text = buildAnnotatedString {
@@ -72,4 +78,17 @@ fun QuestionTracker(counter: Int = 10, outOf: Int = 100) {
             }
         }
     }, modifier = Modifier.padding(20.dp))
+}
+
+@Composable
+fun DrawDottedLine(effect: PathEffect) {
+    Canvas(modifier = Modifier.fillMaxWidth().height(1.dp)) {
+        drawLine(
+            color = AppColors.mLightGray,
+            start = Offset(0f,0f),
+            end = Offset(size.width, 0f),
+            pathEffect = effect
+        )
+    }
+
 }

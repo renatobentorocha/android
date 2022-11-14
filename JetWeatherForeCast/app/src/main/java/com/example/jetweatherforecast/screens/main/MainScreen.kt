f2df596ebf1c682366bd8651adb3ddae436afa61
reanatorocha <87495675+reanatorocha@users.noreply.github.com>
2022-11-13 23:35:51 -0300

    Weather - Main screen top circle

--- JetWeatherForeCast/app/src/main/java/com/example/jetweatherforecast/screens/main/MainScreen.kt
+++ JetWeatherForeCast/app/src/main/java/com/example/jetweatherforecast/screens/main/MainScreen.kt
@@ -1,16 +1,26 @@
 package com.example.jetweatherforecast.screens.main
 
 import android.util.Log
-import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.produceState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 import com.example.jetweatherforecast.data.DataOrException
 import com.example.jetweatherforecast.model.Weather
 import com.example.jetweatherforecast.widgets.WeatherAppBar
@@ -19,7 +29,7 @@ import com.example.jetweatherforecast.widgets.WeatherAppBar
 fun MainScreen(navController: NavController, mainViewModel: MainViewModel = hiltViewModel()) {
     val weather = produceState<DataOrException<Weather, Boolean, Exception>>(
         initialValue = DataOrException(loading = true)) {
-        value = mainViewModel.getWeather("Seattle")
+        value = mainViewModel.getWeather("Rio de Janeiro")
     }.value
 
     if (weather.loading == true) {
@@ -49,5 +59,50 @@ fun MainScaffold(data: Weather?, navController: NavController) {
 
 @Composable
 fun MainContent(data: Weather?) {
-    Text(text = data?.city?.name ?: "null")
+    val imageUrl = "https://openweathermap.org/img/wn/${data!!.list[0].weather[0].icon}.png"
+
+    Column(
+        modifier = Modifier
+            .padding(4.dp)
+            .fillMaxWidth(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Nov 29",
+            style = MaterialTheme.typography.caption,
+            color = MaterialTheme.colors.onSecondary,
+            fontWeight = FontWeight.SemiBold,
+            modifier = Modifier.padding(6.dp)
+        )
+        
+        Surface(
+            modifier = Modifier
+                .padding(4.dp)
+                .size(200.dp),
+            shape = CircleShape,
+            color = Color(0xFFFFC400)) {
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                WeatherStateImage(imageUrl)
+                Text(text = "56", style = MaterialTheme.typography.h4, fontWeight = FontWeight.ExtraBold)
+                Text(text = "snow", fontStyle = FontStyle.Italic)
+            }
+        }
+    }
+}
+
+@Composable
+fun WeatherStateImage(imageUrl: String) {
+    AsyncImage(
+        model = ImageRequest
+            .Builder(LocalContext.current)
+            .data(imageUrl)
+            .crossfade(true)
+            .build(),
+        contentDescription = "State icon",
+        modifier = Modifier.size(80.dp)
+    )
 }
